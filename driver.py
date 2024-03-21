0cdd8f18248b5b3e5b34b8fd40cea4a3cb8bcd85
Charlie <charlieedoherty@gmail.com>
2024-03-21 13:39:33 -0500

    Initial Commit

---
+++ driver.py
@@ -0,0 +1,20 @@
+"""
+Author: Charlie Doherty
+KUID: 3115329
+Date: 3/20/24
+Lab: 06
+Last modified: 03/21/24
+Purpose: Create a Binary search Tree with binary nodes; includes add and search methods
+
+Run driver to execute the program
+"""
+
+
+def main():
+    """The main function"""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
