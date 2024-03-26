29006df40f31ffa9445fbf3babd35fafd394d00d
Charlie <charlieedoherty@gmail.com>
2024-03-26 12:39:11 -0500

    Create an executive class and build its framework

--- driver.py
+++ driver.py
@@ -3,17 +3,19 @@ Author: Charlie Doherty
 KUID: 3115329
 Date: 3/20/24
 Lab: 06
-Last modified: 03/21/24
+Last modified: 03/26/24
 Purpose: Create a Binary search Tree with binary nodes; includes add and search methods
 
 Run driver to execute the program
 """
+from executive import Executive
 
 
 def main():
     """The main function"""
 
-    pass
+    my_exec = Executive()
+    my_exec.run()
 
 
 if __name__ == "__main__":
---
+++ executive.py
@@ -0,0 +1,24 @@
+from bst import BST
+
+
+class Executive:
+
+    def __init__(self):
+        """Creates the initial Binary Search Tree"""
+        self.BST = BST()
+
+    def add(self):
+        """Takes a comma separated list and adds to the BST"""
+        pass
+
+    def ordered_print(self):
+        """Prints the tree in a user-specified order"""
+        pass
+
+    def print_menu(self):
+        """Prints the menu options"""
+        pass
+
+    def run(self):
+        """Runs the program through the Executive class"""
+        pass
